4c4175ea84a2bcbe528dc850bec56b73d723d10f
Blake Mealey <blakemealey@gmail.com>
2015-11-22 21:27:18 -0700

    update

--- bot_config.js
+++ bot_config.js
@@ -3,19 +3,19 @@ module.exports = {
 	// Bot setup
 	bots: {
 		// Math bot
-		math-bot: {
+		"math-bot": {
 			keywords: ["math"],
 			api_tokens: ["MeOMOCQVJuNVmy5ncAXawuHc", "DVnsiBDARYberjjyIOBoq6ez"]
 		},
 
 		// Echo bot
-		echo-bot: {
+		"echo-bot": {
 			keywords: ["echo"],
 			api_tokens: ["orP5nPZ5VhVmWXAsZp8UFsGq", "Az0nkJFDDTGoOwI4tvlBGh7W"]
 		},
 
 		// Lua bot
-		lua-bot: {
+		"lua-bot": {
 			keywords: ["lua", "luaadmin"],
 			api_tokens: ["oaO0zwUM72SO4l2FkisKZChY"],
 			settings: {
