e6e84a7506054b386cd9cf86d9af8c215576e2be
Blake Mealey <blakemealey@gmail.com>
2015-11-22 22:55:17 -0700

    update

--- bot-handlers/lua-bot/config.js
+++ bot-handlers/lua-bot/config.js
@@ -1,6 +1,6 @@
 module.exports = {
 	user_admins: ["morgoth", "m0rgoth"],
-	user_limited_admins: [];
+	user_limited_admins: [],
 	user_blacklist: [],
 	lua_blacklist: ["while", "repeat", "huge"],
 	max_length: 140
